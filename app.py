156466395816f98810004796a9e163c8d026abee
aryamanchoudhary123 <aryaman.choudhary01@gmail.com>
2021-07-07 21:04:50 +0530

    updated app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def hello_world1():
+
     return render_template('index/index.html')
 
 if __name__ == "__main__":
